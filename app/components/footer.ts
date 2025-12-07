cdda1ba92146778b0246b2be0a5b83ac1088285a
Jonathan Herrera <test@example.com>
2025-12-07 03:34:04 +0100

    Update version in footer

--- app/components/footer.ts
+++ app/components/footer.ts
@@ -5,7 +5,7 @@ export const createFooter = () => {
     'footer',
     `
     <footer id="footer">
-      Version <strong>1.0.0</strong>
+      Version <strong>1.1.0</strong>
       <br>Dieses Programm ist Open Source und kann kostenlos verwendet werden.
       <br><a href="https://github.com/jnthnhrrr/swissDouble/" target="_blank" rel="noopener noreferrer">Quellcode</a> | <a href="https://github.com/jnthnhrrr/swissDouble/blob/master/SPEZIFIKATIONEN.md" target="_blank" rel="noopener noreferrer">Spezifikationen</a> | <a href="https://github.com/jnthnhrrr/swissDouble/blob/master/LICENSE.md" target="_blank" rel="noopener noreferrer">Lizenz</a>
     </footer>
